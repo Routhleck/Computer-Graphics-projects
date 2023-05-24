48a60c398902554cbda4528e6e5aed186b3d5e2d
Routhleck <1310722434@qq.com>
2023-05-24 10:31:33 +0800

    update

--- project1/Sichao He/project1/project1/test.cpp
+++ project1/Sichao He/project1/project1/test.cpp
@@ -6,13 +6,21 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 void processInput(GLFWwindow *window);
 
-// 三角形三个定点的3d坐标， 深度都为0一样
+// 两个三角形的顶点
 float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+	0.5f, 0.5f, 0.0f,   // 右上角
+	0.5f, -0.5f, 0.0f,  // 右下角
+	-0.5f, -0.5f, 0.0f, // 左下角
+	-0.5f, 0.5f, 0.0f   // 左上角
 };
 
+unsigned int indices[] = {
+	// 注意索引从0开始! 
+	// 此例的索引(0,1,2,3)就是顶点数组vertices的下标，
+	// 这样可以由下标代表顶点组合成矩形
+	0, 1, 3, // 第一个三角形
+	1, 2, 3  // 第二个三角形
+};
 const char *vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
@@ -93,6 +101,16 @@ int main()
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
 
+	// EBO
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
 	// shader program (used for link two shader above)
 	unsigned int shaderProgram;
 	shaderProgram = glCreateProgram();
@@ -114,7 +132,8 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	// glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -127,8 +146,9 @@ int main()
 
 		// draw our first triangle
 		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
 		// check and call, switch buffer
 		glfwSwapBuffers(window);
@@ -146,8 +166,19 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	glViewport(0, 0, width, height);
 }
 
+bool ifLine = true;
 void processInput(GLFWwindow *window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+	else if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS) {
+		if (!ifLine) {
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+			ifLine = !ifLine;
+		}
+		else {
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+			ifLine = !ifLine;
+		}
+	}
 }

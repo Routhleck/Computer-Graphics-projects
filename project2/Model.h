6507aefde33a36c4993b54d04976ccb0cfc937f8
F1reC <87696691+F1reC@users.noreply.github.com>
2023-06-11 11:16:37 +0800

    注释 Model.h

--- project2/Model.h
+++ project2/Model.h
@@ -165,49 +165,62 @@ private:
         return Mesh(vertices, indices, textures);
     }
 
-    // checks all material textures of a given type and loads the textures if they're not loaded yet.
-    // the required info is returned as a Texture struct.
-    vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, string typeName)
+// 检查给定类型的所有材质纹理，并在尚未加载的情况下加载纹理。
+// 返回所需的信息作为 Texture 结构。
+vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, string typeName)
+{
+    vector<Texture> textures;
+    
+    // 遍历材质中给定类型的所有纹理
+    for (unsigned int i = 0; i < mat->GetTextureCount(type); i++)
     {
-        vector<Texture> textures;
-        for (unsigned int i = 0; i < mat->GetTextureCount(type); i++)
+        aiString str;
+        mat->GetTexture(type, i, &str);
+        
+        // 检查纹理是否已加载过，如果是，则跳过加载新纹理的步骤
+        bool skip = false;
+        
+        // 遍历已加载的纹理列表，检查文件路径是否相同
+        for (unsigned int j = 0; j < textures_loaded.size(); j++)
         {
-            aiString str;
-            mat->GetTexture(type, i, &str);
-            // check if texture was loaded before and if so, continue to next iteration: skip loading a new texture
-            bool skip = false;
-            for (unsigned int j = 0; j < textures_loaded.size(); j++)
+            if (std::strcmp(textures_loaded[j].path.data(), str.C_Str()) == 0)
             {
-                if (std::strcmp(textures_loaded[j].path.data(), str.C_Str()) == 0)
-                {
-                    textures.push_back(textures_loaded[j]);
-                    skip = true; // a texture with the same filepath has already been loaded, continue to next one. (optimization)
-                    break;
-                }
-            }
-            if (!skip)
-            {   // if texture hasn't been loaded already, load it
-                Texture texture;
-                texture.id = TextureFromFile(str.C_Str(), this->directory);
-                texture.type = typeName;
-                texture.path = str.C_Str();
-                textures.push_back(texture);
-                textures_loaded.push_back(texture);  // store it as texture loaded for entire model, to ensure we won't unnecesery load duplicate textures.
+                textures.push_back(textures_loaded[j]);
+                skip = true; // 文件路径相同的纹理已加载，继续下一个循环（优化）
+                break;
             }
         }
-        return textures;
+        
+        if (!skip)
+        {
+            // 如果纹理尚未加载，则加载它
+            Texture texture;
+            texture.id = TextureFromFile(str.C_Str(), this->directory);
+            texture.type = typeName;
+            texture.path = str.C_Str();
+            textures.push_back(texture);
+            
+            // 将纹理存储为已加载的纹理，以确保不会重复加载冗余纹理。
+            textures_loaded.push_back(texture);
+        }
     }
-};
+    
+    return textures;
+}
 
+// 从文件加载纹理并返回纹理的 ID
 unsigned int TextureFromFile(const char* path, const string& directory, bool gamma)
 {
     string filename = string(path);
     filename = directory + '\\' + filename;
     unsigned int textureID;
+    
+    // 创建纹理对象
     glGenTextures(1, &textureID);
 
     int width, height, nrComponents;
     unsigned char* data = stbi_load(filename.c_str(), &width, &height, &nrComponents, 0);
+    
     if (data)
     {
         GLenum format;
@@ -218,23 +231,26 @@ unsigned int TextureFromFile(const char* path, const string& directory, bool gam
         else if (nrComponents == 4)
             format = GL_RGBA;
 
+        // 绑定纹理并设置纹理参数
         glBindTexture(GL_TEXTURE_2D, textureID);
         glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
 
+        // 设置纹理环绕和过滤选项
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
+        // 释放图像数据
         stbi_image_free(data);
     }
     else
     {
+        // 如果纹理加载失败，输出错误信息并释放图像数据
         std::cout << "Texture failed to load at path: " << path << std::endl;
         stbi_image_free(data);
     }
 
     return textureID;
 }
-#endif

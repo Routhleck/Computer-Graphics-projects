51c9bd9720514a8c4247f715348df8f191e50907
MianBo-Li <104893174+MianBo-Li@users.noreply.github.com>
2023-06-13 09:47:02 +0800

    Update Model.h

--- project2/Model.h
+++ project2/Model.h
@@ -20,19 +20,19 @@ unsigned int TextureFromFile(const char* path, const string& directory, bool gam
 class Model
 {
 public:
-    // model data 
-    vector<Texture> textures_loaded;	// stores all the textures loaded so far, optimization to make sure textures aren't loaded more than once.
-    vector<Mesh>    meshes;
-    string directory;
+    // 模型数据  
+    vector<Texture> textures_loaded;	// 存储到目前为止加载的所有纹理，并进行优化以确保纹理不会多次加载。
+    vector<Mesh>    meshes;//包含了一个Mesh对象的vector
+    string directory;//需要 3D存储文件路径的目录路径
     bool gammaCorrection;
 
-    // constructor, expects a filepath to a 3D model.
+    // 构造函数，需要 3D 模型的文件路径
     Model(const char* path, bool gamma = false) : gammaCorrection(gamma)
     {
         loadModel(path);
     }
 
-    // draws the model, and thus all its meshes
+    // 绘制模型及其所有网格，遍历了所有网格，并调用它们各自的Draw函数。
     void Draw(Shader* shader)
     {
         for (unsigned int i = 0; i < meshes.size(); i++)
@@ -40,62 +40,67 @@ public:
     }
 
 private:
-    // loads a model with supported ASSIMP extensions from file and stores the resulting meshes in the meshes vector.
-    void loadModel(string const& path)
+    // 从文件加载具有受支持的 ASSIMP 扩展名的模型，并将生成的网格存储在meshes vector中。
+    void loadModel(string const& path)//使用Assimp来加载模型至Assimp的一个叫做scene的数据结构中
     {
         // read file via ASSIMP
-        Assimp::Importer importer;
+        Assimp::Importer importer;//首先声明了Assimp命名空间内的一个Importer
+        //调用了它的ReadFile函数，这个函数需要一个文件路径，它的第二个参数是一些后期处理(Post-processing)的选项
+        //通过设定aiProcess_Triangulate，我们告诉Assimp，如果模型不是（全部）由三角形组成，它需要将模型所有的图元形状变换为三角形
+        //aiProcess_FlipUVs将在处理的时候翻转y轴的纹理坐标
+        //aiProcess_CalcTangentSpace计算导入网格的切线和副切线。
+        //aiProcess_GenSmoothNormals 为网格中的所有顶点生成平滑法线。
         const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_GenSmoothNormals| aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
-        // check for errors
+         // 检查错误
         if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) // if is Not Zero
         {
             cout << "ERROR::ASSIMP:: " << importer.GetErrorString() << endl;
             return;
         }
-        // retrieve the directory path of the filepath
+        // 检索文件路径的目录路径
         directory = path.substr(0, path.find_last_of('\\'));
 
-        // process ASSIMP's root node recursively
+        // 递归处理 ASSIM 的根节点
         processNode(scene->mRootNode, scene);
     }
 
-    // processes a node in a recursive fashion. Processes each individual mesh located at the node and repeats this process on its children nodes (if any).
+    // 以递归方式处理节点。处理位于节点上的每个网格，并在其子节点（如果有）上重复此过程。
     void processNode(aiNode* node, const aiScene* scene)
     {
-        // process each mesh located at the current node
+       // 处理位于当前节点的每个网格
         for (unsigned int i = 0; i < node->mNumMeshes; i++)
         {
-            // the node object only contains indices to index the actual objects in the scene. 
-            // the scene contains all the data, node is just to keep stuff organized (like relations between nodes).
+            // 节点对象仅包含索引以索引场景中的实际对象。
+            // 场景包含所有数据，节点只是为了保持东西井井有条（比如节点之间的关系）。
             aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
             meshes.push_back(processMesh(mesh, scene));
         }
-        // after we've processed all of the meshes (if any) we then recursively process each of the children nodes
+       // 处理完所有网格（如果有）后，我们递归处理每个子节点
         for (unsigned int i = 0; i < node->mNumChildren; i++)
         {
             processNode(node->mChildren[i], scene);
         }
 
     }
-
+     //访问网格的相关属性并将它们储存到我们自己的对象中
     Mesh processMesh(aiMesh* mesh, const aiScene* scene)
     {
-        // data to fill
-        vector<Vertex> vertices;
-        vector<unsigned int> indices;
-        vector<Texture> textures;
+        // 要填充的数据
+        vector<Vertex> vertices;//顶点数据
+        vector<unsigned int> indices;//网格索引
+        vector<Texture> textures;//材质数据
 
-        // walk through each of the mesh's vertices
+        // 遍历网格的每个顶点
         for (unsigned int i = 0; i < mesh->mNumVertices; i++)
         {
             Vertex vertex;
-            glm::vec3 vector; // we declare a placeholder vector since assimp uses its own vector class that doesn't directly convert to glm's vec3 class so we transfer the data to this placeholder glm::vec3 first.
-            // positions
+            glm::vec3 vector; // 我们声明一个占位符向量，因为 Assimp 使用自己的 Vector 类，该向量类不会直接转换为 GLM 的 VEC3 类，因此我们首先将数据传输到此占位符 glm：：vec3。
+            // 位置
             vector.x = mesh->mVertices[i].x;
             vector.y = mesh->mVertices[i].y;
             vector.z = mesh->mVertices[i].z;
             vertex.Position = vector;
-            // normals
+            // 法线
             if (mesh->HasNormals())
             {
                 vector.x = mesh->mNormals[i].x;
@@ -103,21 +108,20 @@ private:
                 vector.z = mesh->mNormals[i].z;
                 vertex.Normal = vector;
             }
-            // texture coordinates
-            if (mesh->mTextureCoords[0]) // does the mesh contain texture coordinates?
+            // 纹理坐标
+            if (mesh->mTextureCoords[0])// 网格是否包含纹理坐标
             {
                 glm::vec2 vec;
-                // a vertex can contain up to 8 different texture coordinates. We thus make the assumption that we won't 
-                // use models where a vertex can have multiple texture coordinates so we always take the first set (0).
+                // 一个顶点最多可以包含 8 个不同的纹理坐标。因此，我们假设我们不会使用顶点可以有多个纹理坐标的模型，因此我们始终采用第一组 （0）。
                 vec.x = mesh->mTextureCoords[0][i].x;
                 vec.y = mesh->mTextureCoords[0][i].y;
                 vertex.TexCoords = vec;
-                // tangent
+                // 切线
                 vector.x = mesh->mTangents[i].x;
                 vector.y = mesh->mTangents[i].y;
                 vector.z = mesh->mTangents[i].z;
                 vertex.Tangent = vector;
-                // bitangent
+                 // 比坦根
                 vector.x = mesh->mBitangents[i].x;
                 vector.y = mesh->mBitangents[i].y;
                 vector.z = mesh->mBitangents[i].z;
@@ -125,43 +129,43 @@ private:
             }
             else
                 vertex.TexCoords = glm::vec2(0.0f, 0.0f);
-
+            //vertex结构体已经填充好了需要的顶点属性，我们会在迭代的最后将它压入vertices这个vector的尾部。这个过程会对每个网格的顶点都重复一遍。
             vertices.push_back(vertex);
         }
-        // now wak through each of the mesh's faces (a face is a mesh its triangle) and retrieve the corresponding vertex indices.
+       // 现在遍历网格的每个面（面是网格的三角形）并检索相应的顶点索引。
         for (unsigned int i = 0; i < mesh->mNumFaces; i++)
         {
             aiFace face = mesh->mFaces[i];
-            // retrieve all indices of the face and store them in the indices vector
+            // 检索人脸的所有索引并将它们存储在indices中
             if (face.mNumIndices < 3)
                 continue;
             assert(face.mNumIndices == 3);
             for (unsigned int j = 0; j < face.mNumIndices; j++)
                 indices.push_back(face.mIndices[j]);
         }
-        // process materials
+        // 材质
         aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
-        // we assume a convention for sampler names in the shaders. Each diffuse texture should be named
-        // as 'texture_diffuseN' where N is a sequential number ranging from 1 to MAX_SAMPLER_NUMBER. 
-        // Same applies to other texture as the following list summarizes:
+        // 我们假设着色器中的采样器名称具有约定。每个漫反射纹理都应命名为“texture_diffuseN”，
+        // 其中 N 是 1 到 MAX_SAMPLER_NUMBER 之间的序列号。 
+        // 这同样适用于其他纹理，如以下列表所示：
         // diffuse: texture_diffuseN
         // specular: texture_specularN
         // normal: texture_normalN
 
-        // 1. diffuse maps
+       // 1. 漫反射
         vector<Texture> diffuseMaps = loadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse");
         textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
-        // 2. specular maps
+       // 2. 镜面反射
         vector<Texture> specularMaps = loadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
         textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
-        // 3. normal maps
+        // 3. 法线
         std::vector<Texture> normalMaps = loadMaterialTextures(material, aiTextureType_HEIGHT, "texture_normal");
         textures.insert(textures.end(), normalMaps.begin(), normalMaps.end());
-        // 4. height maps
+       // 4. 高度
         std::vector<Texture> heightMaps = loadMaterialTextures(material, aiTextureType_AMBIENT, "texture_height");
         textures.insert(textures.end(), heightMaps.begin(), heightMaps.end());
 
-        // return a mesh object created from the extracted mesh data
+        // 返回从提取的网格数据创建的网格对象
         return Mesh(vertices, indices, textures);
     }
 
